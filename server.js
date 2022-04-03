5b8f5b9ade0bb59cee708bbfa299003f59c00688
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2022-04-03 18:40:16 +0900

    Part 02_07 (bugs fix)

--- server.js
+++ server.js
@@ -47,13 +47,34 @@ app.get('/list', function(request, answer){
     
 });
 
+// 누가 폼에서 /add post 로 요청하면
+// db counter 내의 게시물갯수 라는 이름을 가진 것?을 찾음.
+// 게시물 갯수를 변수에 저장.
+// db.post에 새게시물 기록.
+// id, 제목, 날짜 등을 추가함.
+// 이게 완료가 되면 db.counter 내의 총 게시물 갯수 + 1
+
 app.post('/add', function(요청, 응답){
+    응답.send('전송완료');
     db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
       var 총게시물갯수 = 결과.totalPost;
+      
       db.collection('post').insertOne( { _id : (총게시물갯수 + 1), 제목 : 요청.body.title, 날짜 : 요청.body.date } , function(){
         console.log('저장완료')
-        응답.send('전송완료');
+
+        //operator
+        //$set : 바꿔달라는 요청을 할 때 쓰는 연산자
+        //$inc : 기존값에 값을 더 해줄때 쓰는 연산자
+
+        //counter 라는 콜렉션에 있는 totalPost 라는 항목도 1 증가시켜야함 (수정);
+        db.collection('counter').updateOne({name:'게시물갯수'},{ $inc : {totalPost:1} },function(error, result){
+            if(error){return console.log(error)}
+
+        });
       });
+
+
+
     });
     
   });
